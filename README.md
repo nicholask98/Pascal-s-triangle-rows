cb262e14536060c4c4b5907c0a5e5a38d4934165
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-02-15 18:28:35 -0800

    Just jotting some initial notes down

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Pascal's-triangle-rows
  
+### Challenge:
+Write a Python function that prints out the first n rows of Pascal's triangle.
+
+Each number is the two numbers above it added together
+
+* Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal.
---
+++ main.py
@@ -0,0 +1,18 @@
+# This was just a brainstorm vvv you can start over
+
+def first_pascal_rows(n):
+    for row in range(n)
+        num_boxes = row + 1
+
+        if box_x > 
+
+
+
+# Goal Like this:
+#           1
+#          1 1
+#         1 2 1
+#        1 3 3 1
+#       1 4 6 4 1
+#     1 5 10 10 5 1 
+#   1 6 15 20 15 6 1
