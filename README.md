591ebea7d84b6b48db242a2dccd0630b803af725
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-02-18 10:16:43 -0800

    Eureka Update
    Check Readme for details.

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ Each number is the two numbers above it added together
 
 * Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal.
 
-## Most Recent Update:
+## Most Recent Update: EUREKA!
+
+It's a little hacky but I got the function to produce the correct output! I start the function pre-for-loop with a print(1) to get the correct first line, and I have the nested if statement print a 1 before iterating through the row_list_current. Does not print as a list any more due to the nested for loop.
+
+All that's left is to align in aesthetically!
+
+
+### Last Update:
+
 It works now except for the first 1 in each line and the correct spacing.
 Also, it still has the []'s from the lists and it would be better if it didn't.
--- main.py
+++ main.py
@@ -1,53 +1,31 @@
-'''
-def first_pascal_rows(num_rows):
+def pascals_tri(num_rows):
     row_list_former = []
     row_list_current = []
+    print(1)
+    for row_num in range(num_rows):# FOR NEW ROWS        
+        for current_num in range(len(row_list_former) + 1): # FOR NUM IN CURRENT ROW            
+            if current_num == len(row_list_former):
+                row_list_current.append(1)
+                print(1, end = ' ')
+                for this_num in row_list_current:
+                    print(this_num, end = ' ')
+                print()
+                break
 
-    for num in range(num_rows): # Veritcally down ****
-        if num == 0:
-            # 1 
-        elif num == 1:
-            # row_number (0, 1, 2, 3 // not 1, 2, 3)
-        elif num == 2:
-            # 
-
-        print(num_rows)
-'''
-
-
-
-num_rows = int(input('enter whole number:\n'))
-
-row_list_former = []
-row_list_current = []
-
-for row_num in range(num_rows):# FOR NEW ROWS
-    for current_num in range(len(row_list_former) + 1): # FOR NUM IN CURRENT ROW
-        
-        if current_num == len(row_list_former):
-            row_list_current.append(1)
-            print(row_list_current) 
-            break
-
-        row_list_current.append(row_list_former[current_num - 1] + row_list_former[current_num])
-
-      
-    row_list_former = row_list_current
-    row_list_current = []
+            row_list_current.append(row_list_former[current_num - 1] + row_list_former[current_num])
+        row_list_former = row_list_current
+        row_list_current = []
     
+user_num = input('Enter a whole number or enter q to quit:\n')
+while user_num != 'q':
+    if not user_num.isdigit():
+        user_num = input('Enter a whole number or enter q to quit:\n')
+        continue
+    pascals_tri(int(user_num))
+    user_num = input('Enter a whole number or enter q to quit:\n')
 
-
-
+print('See ya!')
 # Goal Like this for input(8): ****
-
-# 1
-# 1 1
-# 1 2 1
-# 1 3 3 1
-# 1 4 6 4 1
-# 1 5 10 10 5 1 
-# 1 6 15 20 15 6 1
-# 1 7 21 35 35 21 7 1
 
 
 
@@ -61,3 +39,18 @@ for row_num in range(num_rows):# FOR NEW ROWS
 #     1 5 10 10 5 1 
 #   1 6 15 20 15 6 1
 #  1 7 21 35 35 21 7 1
+
+
+
+# Num spaces before line = absolute value(reverse position of list index) - 1
+#           x [-11] spaces: 10
+#          x x [-10] spaces: 9
+#         x x x [-9] spaces: 8
+#        x x x x [-8] spaces: 7
+#       x x x x x [-7] spaces: 6
+#      x x x x x x [-6] spaces: 5
+#     x x x x x x x [-5] spaces: 4
+#    x x x x x x x x [-4] spaces: 3
+#   x x x x x x x x x [-3] spaces: 2
+#  x x x x x x x x x x [-2] spaces: 1
+# x x x x x x x x x x x  [-1] spaces: 0
